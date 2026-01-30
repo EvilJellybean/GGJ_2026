5ae4682012de367375cd82dda0dcf5d85164bf2c
Amos Wolfe <amos@dropbearbytes.com>
2026-01-30 20:55:03 +0800

    Started on enemy script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -24,8 +24,6 @@ public class PlayerMovement : MonoBehaviour
         Vector2 moveVector = moveInput.action.ReadValue<Vector2>();
         Vector3 moveAmount = new Vector3(moveVector.x, 0.0f, moveVector.y);
 
-        Debug.Log(moveVector);
-
         rigidbody.MovePosition(rigidbody.position + moveAmount * moveSpeed * Time.deltaTime);
     }
 }
