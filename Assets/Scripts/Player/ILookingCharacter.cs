f8f0201cdf557023b7cbb9d69b6568fe866618bc
Amos Wolfe <amos@dropbearbytes.com>
2026-01-31 14:53:49 +0800

    Added character sprite component to enemy

---
+++ Assets/Scripts/Player/ILookingCharacter.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface ILookingCharacter
+{
+    public Vector2 LookDirection { get; }
+}
--- Assets/Scripts/Player/IMovingCharacter.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IMovingCharacter
-{
-    public Vector2 MoveDirection { get; }
-}
